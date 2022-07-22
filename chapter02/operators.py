c75c1a0bfbb92a0135d51f31bab1f40ea90425e8
unknown <rhbappy456@gmail.com>
2022-07-22 12:12:23 +0600

    variable, data types & operators

---
+++ chapter02/operators.py
@@ -0,0 +1,35 @@
+a = 3
+b = 4
+#Arithmetic Operators:
+print("the value of 3+4 is:", a+b) #Addition
+print("the value of 3-4 is:", a-b) #Subtraction
+print("the value of 3*4 is:", a*b) #Multiplication
+print("the value of 3/4 is:", a/b) #Division
+print("the value of 3%4 is:", a%b) #Modulus
+print("the value of 3**4 is:", a**b) #Exponent
+print("the value of 3//4 is:", a//b) #Floor Division
+
+#Assignment Operators:
+a = 34
+b = 34
+a += 12
+b -= 12
+print("the value of a is:", a) # 46
+print("the value of b is:", b)  # 22
+
+#Comparison Operators:
+a = (4 > 6)
+b = (4 < 6)
+c = (4 == 6)
+d = (4 != 6)
+print("the value of 4 > 6 is:", a) # False
+print("the value of 4 < 6 is:", b) # True
+print("the value of 4 == 6 is:", c) # False
+print("the value of 4 != 6 is:", d) # True
+
+#Logical Operators:
+bool1 = True
+bool2 = False
+print("the value of bool1 and bool2 is:", (bool1 and bool2)) # False
+print("the value of bool1 or bool2 is:", (bool1 or bool2)) # True
+print("the value of not bool2 is:", not bool2) # True
---
+++ chapter02/variable.py
@@ -0,0 +1,23 @@
+# Defining variable 
+a = "Rakib"
+b =345
+c= 45.32
+d = True
+e = "Hello"
+f = None
+
+# Printing the variables:
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+# type of variable b:
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
