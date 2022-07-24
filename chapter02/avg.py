5995dfd8f73ee97ced1cd11c38b416ea6b55beeb
unknown <rhbappy456@gmail.com>
2022-07-24 09:32:16 +0600

    avg two number

---
+++ chapter02/avg.py
@@ -0,0 +1,4 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+avg = (int(a) + int(b)) / 2  # convert string to int(if possible)
+print("Average is:", avg) # print the average
