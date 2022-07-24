2a13eb3afcb97b6cc2228723bf7b0a55a3cf52c5
unknown <rhbappy456@gmail.com>
2022-07-24 11:41:42 +0600

    topic numbering & solved some problem

---
+++ chapter04/prob01_store_fruits.py
@@ -0,0 +1,10 @@
+f1 = input("Enter the first fruit Number 1: ")
+f2 = input("Enter the first fruit Number 2: ")
+f3 = input("Enter the first fruit Number 3: ")
+f4 = input("Enter the first fruit Number 4: ")
+f5 = input("Enter the first fruit Number 5: ")
+f6 = input("Enter the first fruit Number 6: ")
+f7 = input("Enter the first fruit Number 7: ")
+
+myFruitList = [f1, f2, f3, f4, f5, f6, f7]
+print("My fruit list: ", myFruitList)
