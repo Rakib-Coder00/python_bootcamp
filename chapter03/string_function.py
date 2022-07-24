b46bc91133499554a8ce4f4cf6890e5200f26df7
unknown <rhbappy456@gmail.com>
2022-07-24 10:18:26 +0600

    string_function

---
+++ chapter03/string_function.py
@@ -0,0 +1,12 @@
+story = "Blah ! Blah! Blah! RakibIsBad"
+
+#String Functions:
+# print(len(story))
+# print(story.endswith("!"))
+# print(story.startswith("Blah"))
+# print(story.find("Blah"))
+# print(story.count("B"))
+# print(story.upper())
+# print(story.lower())
+# print(story.capitalize())
+print(story.replace("Blah", "RHB"))
