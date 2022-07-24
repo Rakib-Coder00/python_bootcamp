e9a7ad662dae5523c4e7a33112fdbb1fdf08f088
unknown <rhbappy456@gmail.com>
2022-07-24 20:59:49 +0600

    chapter 05 practice set & solution

---
+++ chapter05/prob6.py
@@ -0,0 +1,10 @@
+favlang= {}
+a = input("Enter your favorite language a\n")
+b = input("Enter your favorite language b\n")
+c = input("Enter your favorite language c\n")
+d = input("Enter your favorite language d\n")
+favlang["friend1"] = a
+favlang["friend2"] = b
+favlang["friend3"] = c
+favlang["friend4"] = d
+print(favlang)
