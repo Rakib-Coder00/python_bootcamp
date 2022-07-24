95fba97001c6358cc5cacb2d16f1dbdbb2044247
unknown <rhbappy456@gmail.com>
2022-07-24 11:03:40 +0600

    list & list slicing

---
+++ chapter04/list.py
@@ -0,0 +1,16 @@
+#Create a list using []
+a = [1, 2, 3, 4, 5]
+print(a[0])
+
+#Print the list using print() function
+print(a)
+
+#Access the list using index
+print(a[2])
+
+#Change the value of the list using index
+a[0] = 98
+print(a)
+
+#We can create a list with items of a different types
+b = [1, "two", 3, "four", 5, True]
---
+++ chapter04/list_slicing.py
@@ -0,0 +1,3 @@
+#List slicing:
+friends = ["John", "Paul", "Ringo", "George", 47, 69]
+print(friends[0:3]) 
