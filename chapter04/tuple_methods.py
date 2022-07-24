a17d4489964bb9541a4e2f99fcc123ebe85cd822
unknown <rhbappy456@gmail.com>
2022-07-24 11:30:53 +0600

    tuple_methods

---
+++ chapter04/tuple_methods.py
@@ -0,0 +1,4 @@
+#creating a tuple using () 
+t = (1, 2, 3, 4, 5, 1, 1, 2 ,2)
+print(t.count(1))
+print(t.index(1))
---
+++ chapter04/tuples.py
@@ -0,0 +1,12 @@
+#creating a tuple using () 
+t = (1, 2, 3, 4, 5)
+# t1 = () #empty tuple
+# t1 = (1) # Wrong way to declare a tuple with only one element
+t1 = (1,) #tuple with one element
+print(t1)
+
+# Printing the elements of a tuple
+print(t[0])
+
+#can't update the values of a tuple
+# t[0] = 10 #will give an error
