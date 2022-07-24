8ff3380ee9ff49f341c08264f37e0d8da18a2d76
unknown <rhbappy456@gmail.com>
2022-07-24 10:02:17 +0600

    string & string slicing

---
+++ chapter03/string_slicing.py
@@ -0,0 +1,23 @@
+name = "Python"
+# greeting = "Hello,"
+# print(type(name))
+
+# Concatenating two strings:
+# print(greeting + " " + name)
+
+print(name[0])
+print(name[1])
+print(name[-1])
+
+# name[2] = "X" # This will give error
+
+# String Slicing:
+print(name[0:3]) # This will print "Pyt"
+print(name[:3]) # This will start from the beginning and print "Pyt"
+print(name[3:]) # This will print from 3rd index to end
+print(name[-3:]) # This will print from last 3rd index to end
+
+
+
+
+
---
+++ chapter03/strings.py
@@ -0,0 +1,6 @@
+a = 22
+b = "Rakib"
+c = '''Rakib'viper'''  # Use this if you have single quotes in your string
+print(type(a))
+print(type(b))
+print(type(c))
