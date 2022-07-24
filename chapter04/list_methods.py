247ae31d0cc7ae259c8f5b8cfd22524bdad33c7a
unknown <rhbappy456@gmail.com>
2022-07-24 11:18:15 +0600

    list methods

---
+++ chapter04/list_methods.py
@@ -0,0 +1,8 @@
+l1= [1, 8, 7, 21, 15]
+# l1.sort() #sort the list in ascending order
+# l1.reverse() #reverse the list
+# l1.append(23) #add an item to the end of the list
+# l1.insert(2, 10) #insert an item at a specific index 
+# l1.pop() #remove the last item from the list
+# l1.remove(8) #remove an item from the list  
+print(l1)
