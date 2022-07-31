fcd1ff85620eb7eae8660f4a963411e21cabde6b
unknown <rhbappy456@gmail.com>
2022-07-31 10:07:24 +0600

    practice set added

---
+++ chapter11/prob02.py
@@ -0,0 +1,13 @@
+class Animals:
+    animaType = "Mammal"
+
+class Pets:
+    color = "White"
+
+class Dog:
+    @staticmethod
+    def bark():
+        print("Woof!")  
+
+d = Dog()
+d.bark()          
